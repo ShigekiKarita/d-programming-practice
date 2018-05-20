eba80fa87bcaf8210c561cb909edb6d7b14c66dd
karita <shigekikarita@gmail.com>
2018-05-21 01:06:31 +0900

    update

--- judge.d
+++ judge.d
@@ -3,7 +3,8 @@ import std.stdio;
 import std.file : dirEntries, SpanMode, tempDir, readText, exists;
 import std.path : dirName, baseName, stripExtension, buildPath;
 import std.algorithm : each, filter, startsWith;
-import std.string : replace;
+import std.string : replace, split;
+import std.conv : to;
 
 enum usage = `USAGE: rdmd judge.d ./foo/bar.d
 
@@ -13,6 +14,15 @@ NOTE: you must have question & answer txt like
 ./foo/bar.q2 ./foo/bar.a2
 ...
 `;
+
+size_t readGcBytes(string path) {
+    size_t ret = 0;
+    foreach (line; File(path).byLine) {
+        if (line.startsWith("bytes allocated")) continue;
+        ret += line.split()[0].to!size_t;
+    }
+    return ret;
+}
 
 int main(string[] args) {
     if (args.length != 2) {
@@ -31,14 +41,22 @@ int main(string[] args) {
     immutable dname = problem.dirName;
     void check(in string qfile) {
         import std.process : spawnProcess, wait;
+        import std.datetime.stopwatch;
         writefln("- judging %s", qfile);
         immutable qbase = qfile.baseName;
         immutable abase = qbase.replace(".q", ".a");
         immutable afile = dname.buildPath(abase);
-        auto cmd = ["rdmd", codeName];
+        /*
+          see: https://beta.atcoder.jp/contests/practice/rule
+          D (DMD64 v2.070.1)	dmd -m64 -w -O -release -inline ./Main.d
+          D (LDC 0.17.0)	ldc2 -O ./Main.d -of ./a.out
+         */
+        auto cmd = ["rdmd", "-m64", "-profile=gc", codeName];
         auto sout = File(tempDir.buildPath(qbase ~ ".out"), "w");
         auto serr = File(tempDir.buildPath(qbase ~ ".err"), "w");
+        auto swatch = StopWatch(AutoStart.yes);
         immutable result = wait(spawnProcess(cmd, File(qfile), sout, serr));
+        immutable time = swatch.peek();
         assert(result == 0, ">>> ERROR\n" ~ serr.name.readText);
         immutable answer = sout.name.readText;
         immutable expected = afile.readText;
@@ -48,8 +66,13 @@ int main(string[] args) {
             write("> answer:\n", answer);
             write("> expected:\n", expected);
         }
-        writeln("---------------------");
-        
+        writefln("- time: %s", time);
+        enum gclog = "profilegc.log";
+        if (gclog.exists) {
+            writefln("- memory: %d bytes", gclog.readGcBytes);
+        }
+        writeln(serr.name.readText);
+        writeln("---------------------");        
     }
 
     writefln("====== running %s ======", bname);
--- test-judge/test.d
+++ test-judge/test.d
@@ -4,5 +4,6 @@ void main() {
     int a, b, c;
     string s;
     readf("%d\n%d %d\n%s\n", &a, &b, &c, &s);
+    auto x = new int[30];
     writef("%d %s\n", a + b + c, s);
 }
